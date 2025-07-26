a736d0400305cf3e9fda7195126a54f9f14772db
nirmal-k-r <test@tester.com>
2025-07-26 13:27:21 +0400

    new

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ server.use((err, req, res, next) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000; //set port to environment variable or default to 3000
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
